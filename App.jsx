c1b8879afeddd115d78b2ee4441f6db587b0a442
Al Saef Ratul <110528853+Alsaef@users.noreply.github.com>
2025-04-04 20:20:24 +0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// App.js or HomeScreen.jsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hello from JSX!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
